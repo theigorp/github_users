3c3dd3f5d564e9791547cb3f40122c3e9eb8a908
theigorp <mr.igor187@gmail.com>
2024-03-08 10:58:35 +0100

    FEAT: add functionality to toggle color theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   faMoon = faMoon;
   faSun = faSun;
+  themeText: 'light' | 'dark' = 'light';
+
+  onToggleTheme(): void {
+    if (this.themeText === 'dark') {
+      this.themeText = 'light';
+    } else {
+      this.themeText = 'dark';
+    }
+  }
 }
