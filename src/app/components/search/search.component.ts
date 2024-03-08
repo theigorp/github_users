877accfa5e75caa05c89266799d1dc0c29505aca
theigorp <mr.igor187@gmail.com>
2024-03-08 12:58:28 +0100

    FEAT: get search query value on submit

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, FormsModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
+  @ViewChild('usernameInput') usernameInputRef!: ElementRef;
   faMagnifyingGlass = faMagnifyingGlass;
   faX = faX;
   errorMessage = '';
+
+  onSearchUsername() {
+    const searchedUsername = this.usernameInputRef.nativeElement.value;
+  }
 }
