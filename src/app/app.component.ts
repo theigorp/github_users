eb0740a52de0727a04b0c8a0f5b013862cad822b
theigorp <mr.igor187@gmail.com>
2024-03-18 08:30:49 +0100

    FEAT: generate user-profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SearchComponent,
+    UserProfileComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss',
+})
+export class UserProfileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
