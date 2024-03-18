444898e924fbf32f13d35f7cd717d3ec6d36f706
theigorp <mr.igor187@gmail.com>
2024-03-18 09:40:46 +0100

    FEAT: fetch user info on startup and search, pass info to user-profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +20,24 @@ import { UserProfileComponent } from './components/user/user-profile/user-profil
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  user!: User;
+
+  // ideally, this logic should be located in user-profile.component.ts where oninit it will
+  // get 'theigor' userinfo and display it, and then every subsequent search will trigger new
+  // request. But for now, I chose to go with emitting the search event and passing props
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getRequestedUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  onSearch() {
+    this.userService.getRequestedUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
+}
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { UserService } from './../../services/user.service';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -12,12 +19,17 @@ import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
+  @Output() search = new EventEmitter();
   @ViewChild('usernameInput') usernameInputRef!: ElementRef;
   faMagnifyingGlass = faMagnifyingGlass;
   faX = faX;
   errorMessage = '';
 
+  constructor(private userService: UserService) {}
+
   onSearchUsername() {
     const searchedUsername = this.usernameInputRef.nativeElement.value;
+    this.userService.setRequestedUser(searchedUsername);
+    this.search.emit();
   }
 }
