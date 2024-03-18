80346fdd7606e4887bb1975123ff52319237b492
theigorp <mr.igor187@gmail.com>
2024-03-18 12:37:03 +0100

    FEAT: style the user-profile component

--- src/app/components/user/user-profile/user-profile.component.ts
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,8 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
-export class UserProfileComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class UserProfileComponent {
+  @Input() user!: User;
+  // create an interface for when a user is not found => we receive { message: 'not found', documentation_url: string }
 }
