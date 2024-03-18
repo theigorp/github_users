a2800921f172a7ee03e3d1d826908a9e25cb1182
theigorp <mr.igor187@gmail.com>
2024-03-18 17:48:57 +0100

    FEAT: create structure for socials component

--- src/app/components/user/profile-socials/profile-socials.component.ts
+++ src/app/components/user/profile-socials/profile-socials.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+  faLocationDot,
+  faLink,
+  faBuilding,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile-socials',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './profile-socials.component.html',
-  styleUrl: './profile-socials.component.scss'
+  styleUrl: './profile-socials.component.scss',
 })
 export class ProfileSocialsComponent {
-
+  @Input() location!: string | null;
+  @Input() xHandle!: string | null;
+  @Input() website!: string | null;
+  @Input() company!: string | null;
+  faLocation = faLocationDot;
+  faWebsite = faLink;
+  faTwitter = faSquareXTwitter;
+  faCompany = faBuilding;
 }
