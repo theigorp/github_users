e88bf31de691b43ada0b52d743823d64f25fc507
theigorp <mr.igor187@gmail.com>
2024-03-18 18:06:11 +0100

    FEAT: style profile-socials

--- src/app/components/user/profile-socials/profile-socials.component.ts
+++ src/app/components/user/profile-socials/profile-socials.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
 import {
@@ -10,7 +11,7 @@ import {
 @Component({
   selector: 'app-profile-socials',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './profile-socials.component.html',
   styleUrl: './profile-socials.component.scss',
 })
