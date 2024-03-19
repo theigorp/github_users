8014013aa4a04d3de85500656145c60c941a45b8
theigorp <mr.igor187@gmail.com>
2024-03-19 06:25:17 +0100

    FEAT: set query params when searching users

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -29,7 +30,7 @@ export class SearchComponent implements OnChanges {
   faX = faX;
   errorMessage = 'no results';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['requestFailed'].currentValue === true) {
@@ -40,10 +41,12 @@ export class SearchComponent implements OnChanges {
   onSearchUsername() {
     const searchedUsername = this.usernameInputRef.nativeElement.value;
     this.userService.setRequestedUser(searchedUsername);
+    this.router.navigate([], { queryParams: { username: searchedUsername } });
     this.search.emit();
   }
 
   onClearUsername() {
+    this.router.navigate([], { queryParams: null });
     this.usernameInputRef.nativeElement.value = '';
     this.errorMessage = '';
   }
